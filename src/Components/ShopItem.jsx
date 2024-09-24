06420a2694bec50d4e0e4ed951f495099ffcd160
Robotex <ivanivan1001@bk.ru>
2024-09-24 14:24:59 +0300

    Initial commit

---
+++ src/Components/ShopItem.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+export const ShopItem = ({ item }) => {
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">
+          {item.currency}
+          {item.price}.00
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+};
+
+ShopItem.propTypes = {
+  item: PropTypes.shape({
+    brand: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    descriptionFull: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
